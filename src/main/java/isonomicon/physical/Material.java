e058e0267cab23d24e52e9b209c7fd68c8f40c0a
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-05-05 21:07:58 -0700

    More Materials. Document some stuff that could be tricky.

--- src/main/java/isonomicon/physical/Material.java
+++ src/main/java/isonomicon/physical/Material.java
@@ -19,14 +19,25 @@ public class Material {
      * {@link #id}, but for radiant particles this could be 0 (no outline), or a very bright Material to add a halo.
      */
     public final byte outlineID;
-    
+
+    /**
+     * A storage for the probability weights for what this Material may change into over the course of an animation; the
+     * actual Material IDs are stored in {@link #transitionIDs}. This is often not initialized explicitly, in which case
+     * this will store a simple WeightedTable that always returns index 0 (matching the index of the only ID in
+     * transitionIDs under the same circumstance, which is almost always this Material's ID).
+     */
     public final WeightedTable transitions;
+    /**
+     * A storage for the Material IDs that this may change into at each frame of an animation. This is often not
+     * initialized explicitly, in which case this will just store an array containing only this Material's ID.
+     */
     public final byte[] transitionIDs;
     /**
      * If two voxels have different Materials but those Materials have compatible meld fields, then a slope can be drawn
      * connecting them; otherwise, slopes should be blocked from forming. Two Materials are compatible to meld if any
      * bits are shared between this field on both Materials; that is, {@code (materialA.meld & materialB.meld) != 0}.
-     * This helps prevent the appearance of a person "melting onto their clothes" or other problematic slopes.
+     * This helps prevent the appearance of a person "melting onto their clothes" or other problematic slopes. Using a
+     * meld value of {@code -1L} will form slopes with any voxel that can meld at all (its meld must be non-0). 
      */
     public final long meld;
     
@@ -51,6 +62,22 @@ public class Material {
     }
     
     public static final Material[] MATERIALS = new Material[]{
-            new Material("Transparent", 0, 0, 0L)
+            new Material("transparent", 0, 0, 0L),
+            new Material("metal A", 1, 1, 1L),
+            new Material("char", 2, 2, -1L),
+            new Material("mud A", 3, 3, -1L),
+            new Material("wilted flower", 4, 4, 8L),
+            new Material("stone A", 5, 5, 2L),
+            new Material("stone B", 6, 6, 2L),
+            new Material("metal B", 7, 7, 1L),
+            new Material("cloud", 8, 0, 256L),
+            new Material("crystal A", 9, 9, 2L),
+            new Material("cloth A", 10, 10, 16L),
+            new Material("blood", 11, 0, 4L),
+            new Material("guts", 12, 12, 4L),
+            new Material("meat", 13, 11, 4L),
+            new Material("brick", 14, 14, 0L),
+            new Material("cloth B", 15, 15, 16L),
+            
     };
 }
