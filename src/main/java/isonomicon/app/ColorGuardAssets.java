300f6116c0f709a6ff99e71a0fdd531b5978fbdb
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-09-11 21:21:24 -0700

    Submarine!

--- src/main/java/isonomicon/app/ColorGuardAssets.java
+++ src/main/java/isonomicon/app/ColorGuardAssets.java
@@ -57,6 +57,7 @@ public class ColorGuardAssets extends ApplicationAdapter {
                 "Transport_Copter.vox", "palettes/b/ColorGuardBaseDark.png",
                 "Patrol_Boat.vox", "palettes/b/ColorGuardBaseDark.png",
                 "Battleship.vox", "palettes/b/ColorGuardBaseDark.png",
+                "Submarine.vox", "palettes/b/ColorGuardBaseDark.png",
                 "Fighter_Jet.vox", "palettes/b/ColorGuardBaseDark.png",
                 "Stealth_Jet.vox", "palettes/b/ColorGuardBaseDark.png",
                 "Infantry.vox", "palettes/b/ColorGuardBaseDark.png",
