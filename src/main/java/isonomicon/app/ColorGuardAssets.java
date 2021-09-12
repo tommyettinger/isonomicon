0a3228429a4d83a35ba2c7478788ffb1d1bd442a
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-09-11 22:49:39 -0700

    Gunship Copter!
    Also some cleanup on the marker voxels.

--- src/main/java/isonomicon/app/ColorGuardAssets.java
+++ src/main/java/isonomicon/app/ColorGuardAssets.java
@@ -56,6 +56,7 @@ public class ColorGuardAssets extends ApplicationAdapter {
                 "Build_Rig.vox", "palettes/b/ColorGuardBaseDark.png",
                 "Transport_Copter.vox", "palettes/b/ColorGuardBaseDark.png",
                 "Blitz_Copter.vox", "palettes/b/ColorGuardBaseDark.png",
+                "Gunship_Copter.vox", "palettes/b/ColorGuardBaseDark.png",
                 "Patrol_Boat.vox", "palettes/b/ColorGuardBaseDark.png",
                 "Battleship.vox", "palettes/b/ColorGuardBaseDark.png",
                 "Submarine.vox", "palettes/b/ColorGuardBaseDark.png",
