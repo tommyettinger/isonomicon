16708998e8bac7b4b45cc351c237d3e7b6e88f86
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-09-12 20:42:28 -0700

    Missile Sniper.

--- src/main/java/isonomicon/app/ColorGuardAssets.java
+++ src/main/java/isonomicon/app/ColorGuardAssets.java
@@ -73,6 +73,7 @@ public class ColorGuardAssets extends ApplicationAdapter {
                 "Rifle_Sniper_Firing.vox", "palettes/b/ColorGuardBaseDark.png",
                 "Mortar_Sniper.vox", "palettes/b/ColorGuardBaseDark.png",
                 "Mortar_Sniper_Firing.vox", "palettes/b/ColorGuardBaseDark.png",
+                "Missile_Sniper.vox", "palettes/b/ColorGuardBaseDark.png",
                 "City.vox", "palettes/b/ColorGuardBaseDark.png",
         };
         if (!new File("specialized/b/vox/color_guard/" + inputs[0]).exists()) {
