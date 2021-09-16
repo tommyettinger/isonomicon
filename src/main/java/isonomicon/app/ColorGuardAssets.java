f9d4774eaaaf4a7e80d912c7e978f8f4f886b2b8
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-09-15 21:20:05 -0700

    Factory!

--- src/main/java/isonomicon/app/ColorGuardAssets.java
+++ src/main/java/isonomicon/app/ColorGuardAssets.java
@@ -84,6 +84,7 @@ public class ColorGuardAssets extends ApplicationAdapter {
                 "Missile_Sniper.vox", "palettes/b/ColorGuardBaseDark.png",
                 "City.vox", "palettes/b/ColorGuardBaseDark.png",
                 "Mansion.vox", "palettes/b/ColorGuardBaseDark.png",
+                "Factory.vox", "palettes/b/ColorGuardBaseDark.png",
         };
         if (!new File("specialized/b/vox/color_guard/" + inputs[0]).exists()) {
             System.out.println("File not found: specialized/b/vox/color_guard/" + inputs[0]);
