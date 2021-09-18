a4df48e0bccc9f5c0bccfd3294121da3551f4a0c
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-09-17 20:22:38 -0700

    Some fixes to gold, change evolve() ordering.

--- src/main/java/isonomicon/app/ColorGuardAssets.java
+++ src/main/java/isonomicon/app/ColorGuardAssets.java
@@ -136,10 +136,10 @@ public class ColorGuardAssets extends ApplicationAdapter {
                     voxels.grids.add(Tools3D.deepCopy(original.get(j)));
                 }
                 for (int f = 0; f < 4; f++) {
-                    pixmap = renderer.drawModelSimple(voxels, i * 0.25f, 0f, 0f, f, 0, 0, 0);
                     for (int j = 0; j < voxels.grids.size(); j++) {
                         Stuff.evolve(Stuff.STUFFS_B, voxels.grids.get(j), f);
                     }
+                    pixmap = renderer.drawModelSimple(voxels, i * 0.25f, 0f, 0f, f, 0, 0, 0);
                     Texture t = new Texture(pixmap.getWidth(), pixmap.getHeight(), Pixmap.Format.RGBA8888);
                     t.draw(renderer.palettePixmap, 0, 0);
                     FrameBuffer fb = new FrameBuffer(Pixmap.Format.RGBA8888, Gdx.graphics.getBackBufferWidth(), Gdx.graphics.getBackBufferHeight(), false);
