81914cf8b7974b1aa3a5ae0013e0c04d03668716
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-09-18 21:51:59 -0700

    Finally, a mine. Mining Outpost!

--- src/main/java/isonomicon/app/ColorGuardAssets.java
+++ src/main/java/isonomicon/app/ColorGuardAssets.java
@@ -88,6 +88,7 @@ public class ColorGuardAssets extends ApplicationAdapter {
                 "Factory.vox", "palettes/b/ColorGuardBaseDark.png",
                 "Airport.vox", "palettes/b/ColorGuardBaseDark.png",
                 "Farm.vox", "palettes/b/ColorGuardBaseDark.png",
+                "Mining_Outpost.vox", "palettes/b/ColorGuardBaseDark.png",
                 "Oil_Well.vox", "palettes/b/ColorGuardBaseDark.png",
                 "Laboratory.vox", "palettes/b/ColorGuardBaseDark.png",
                 "Hospital.vox", "palettes/b/ColorGuardBaseDark.png",
