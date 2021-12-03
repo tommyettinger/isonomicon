a5176d494903dfe05d465367567ec375fc09046b
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-12-02 21:41:04 -0800

    Cleanup and ball() method.

--- src/main/java/isonomicon/physical/ShapeGenerator.java
+++ src/main/java/isonomicon/physical/ShapeGenerator.java
@@ -1,26 +1,49 @@
 package isonomicon.physical;
 
 public class ShapeGenerator {
-    public static byte[][][] line(byte[][][] into, int startX, int startY, int startZ, int endX, int endY, int endZ, int color){
+
+    public static void writeIfEmpty(byte[][][] into, int x, int y, int z, byte color){
+        if(x >= 0 && y >= 0 && z >= 0 && x < into.length && y < into[x].length && z < into[x][y].length
+                && (color == 0) != (into[x][y][z] == 0))
+            into[x][y][z] = color;
+    }
+
+    public static byte[][][] line(byte[][][] into, int startX, int startY, int startZ, int endX, int endY, int endZ, int color) {
         byte c = (byte) color;
         int dx = endX - startX, dy = endY - startY, dz = endZ - startZ,
                 nx = Math.abs(dx), ny = Math.abs(dy), nz = Math.abs(dz);
         int signX = dx >> 31 | 1, signY = dy >> 31 | 1, signZ = dz >> 31 | 1,
                 workX = startX, workY = startY, workZ = startZ;
-        if(into[workX][workY][workZ] == 0) into[workX][workY][workZ] = c;
+        writeIfEmpty(into, workX, workY, workZ, c);
         for (int i = 1, ix = 0, iy = 0, iz = 0; ix < nx || iy < ny || iz < nz; i++) {
             float x = (0.5f + ix) / nx, y = (0.5f + iy) / ny, z = (0.5f + iz) / nz;
             if (x < y && x < z) {
                 workX += signX;
                 ix++;
-            } else if(y <= x && y < z){
+            } else if (y <= x && y < z) {
                 workY += signY;
                 iy++;
             } else {
                 workZ += signZ;
                 iz++;
             }
-            if(into[workX][workY][workZ] == 0) into[workX][workY][workZ] = c;
+            writeIfEmpty(into, workX, workY, workZ, c);
+        }
+        return into;
+    }
+
+    public static byte[][][] ball(byte[][][] into, int centerX, int centerY, int centerZ, double radius, int color) {
+        byte c = (byte) color;
+        int ceiling = (int)Math.ceil(radius);
+        radius *= radius;
+        for (int x = Math.max(0, centerX - ceiling); x <= Math.min(into.length - 1, centerX + ceiling); x++) {
+            for (int y = Math.max(0, centerY - ceiling); y <= Math.min(into[x].length - 1, centerY + ceiling); y++) {
+                for (int z = Math.max(0, centerZ - ceiling); z <= Math.min(into[x][y].length - 1, centerZ + ceiling); z++) {
+                    int dx = centerX - x, dy = centerY - y, dz = centerZ - z;
+                    if(dx * dx + dy * dy + dz * dz <= radius)
+                        writeIfEmpty(into, x, y, z, c);
+                }
+            }
         }
         return into;
     }
