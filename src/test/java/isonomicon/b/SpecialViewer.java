726f31388c326b0944ceb34afced5af0760e4919
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-06-05 23:16:44 -0700

    The timing on the missile seems wrong.

--- src/test/java/isonomicon/b/SpecialViewer.java
+++ src/test/java/isonomicon/b/SpecialViewer.java
@@ -41,9 +41,9 @@ public class SpecialViewer extends ApplicationAdapter {
 //        String name = "Phantom_Wand";
 //        String name = "Figure";
 
-        String name = "Mortar_Sniper";
+        String name = "Fighter_Jet";
         ColorGuardData.Unit unit = ColorGuardData.byName.get(name);
-        String target = "Infantry";
+        String target = "Blitz_Copter";
 
         images = new Texture[64];
         receiveImages = new Texture[64];
@@ -94,19 +94,35 @@ public class SpecialViewer extends ApplicationAdapter {
         int time = (int) (TimeUtils.timeSinceMillis(startTime) >>> 7);
         batch.draw(images[(time & 7) | (time & 56)], 64.0f, 64.0f);
         time ^= 16;
-        batch.setColor(1f/256f, 0.5f, 0.5f, 1f);
-        batch.draw(targetImages[(time & 3) | (time >>> 1 & 12)], 60 + 64.0f, 30 + 64.0f);
-        batch.setColor(2f/256f, 0.5f, 0.5f, 1f);
-        batch.draw(targetImages[(time & 3) | (time >>> 1 & 12)], -60 + 64.0f, 30 + 64.0f);
-        batch.setColor(3f/256f, 0.5f, 0.5f, 1f);
-        batch.draw(targetImages[(time & 3) | (time >>> 1 & 12)], -60 + 64.0f, -30 + 64.0f);
-        batch.setColor(4f/256f, 0.5f, 0.5f, 1f);
-        batch.draw(targetImages[(time & 3) | (time >>> 1 & 12)], 60 + 64.0f, -30 + 64.0f);
+        if(true) {
+            batch.setColor(1f / 256f, 0.5f, 0.5f, 1f);
+            batch.draw(targetImages[(time & 3) | (time >>> 1 & 12)], 60 + 64.0f, 30 + 64.0f);
+            batch.setColor(2f / 256f, 0.5f, 0.5f, 1f);
+            batch.draw(targetImages[(time & 3) | (time >>> 1 & 12)], -60 + 64.0f, 30 + 64.0f);
+            batch.setColor(3f / 256f, 0.5f, 0.5f, 1f);
+            batch.draw(targetImages[(time & 3) | (time >>> 1 & 12)], -60 + 64.0f, -30 + 64.0f);
+            batch.setColor(4f / 256f, 0.5f, 0.5f, 1f);
+            batch.draw(targetImages[(time & 3) | (time >>> 1 & 12)], 60 + 64.0f, -30 + 64.0f);
 
-        int angle = time >>> 3 & 3;
-        batch.setColor(0f, 0.5f, 0.5f, 1f);
-        batch.draw(receiveImages[(time & 7) | (time & 56)], 60 * (1|-((angle ^ angle >>> 1) & 1)) + 64.0f, 30 * (1 - (angle & 2)) + 64.0f);
+            int angle = time >>> 3 & 3;
+            batch.setColor(0f, 0.5f, 0.5f, 1f);
+            batch.draw(receiveImages[(time & 7) | (time & 56)], 60 * (1 | -((angle ^ angle >>> 1) & 1)) + 64.0f, 30 * (1 - (angle & 2)) + 64.0f);
+        }
+        else{
+            batch.setColor(1f/256f, 0.5f, 0.5f, 1f);
+            batch.draw(targetImages[(time & 3) | (time >>> 1 & 12)], 40 + 64.0f, 20 + 64.0f);
+            batch.setColor(2f/256f, 0.5f, 0.5f, 1f);
+            batch.draw(targetImages[(time & 3) | (time >>> 1 & 12)], -40 + 64.0f, 20 + 64.0f);
+            batch.setColor(3f/256f, 0.5f, 0.5f, 1f);
+            batch.draw(targetImages[(time & 3) | (time >>> 1 & 12)], -40 + 64.0f, -20 + 64.0f);
+            batch.setColor(4f/256f, 0.5f, 0.5f, 1f);
+            batch.draw(targetImages[(time & 3) | (time >>> 1 & 12)], 40 + 64.0f, -20 + 64.0f);
 
+            int angle = time >>> 3 & 3;
+            batch.setColor(0f, 0.5f, 0.5f, 1f);
+            batch.draw(receiveImages[(time & 7) | (time & 56)], 40 * (1|-((angle ^ angle >>> 1) & 1)) + 64.0f, 20 * (1 - (angle & 2)) + 64.0f);
+
+        }
         batch.end();
     }
 
