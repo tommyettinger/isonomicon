770128d63727f9fdbf7c97460e5737ff798dfa3a
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-09-23 14:17:39 -0700

    Roberts dither with lower strength looks nice!
    Dither strength wasn't being set correctly before, I think.

--- src/main/java/isonomicon/app/ColorGuardAssets.java
+++ src/main/java/isonomicon/app/ColorGuardAssets.java
@@ -81,7 +81,7 @@ public class ColorGuardAssets extends ApplicationAdapter {
 //        ColorGuardData.units = ColorGuardData.units.stream().filter(u -> u.hasWeapon("Forward_Missile") || u.hasWeapon("Handgun")).toList();
 //        ColorGuardData.units = ColorGuardData.units.stream().filter(u -> u.name.equals("Submarine")).toList();
 //        ColorGuardData.units = ColorGuardData.units.subList(52, ColorGuardData.units.size());
-        ColorGuardData.units = ColorGuardData.units.subList(0, 4);
+//        ColorGuardData.units = ColorGuardData.units.subList(0, 4);
         try {
             head = VoxIOExtended.readVox(new LittleEndianDataInputStream(new FileInputStream("specialized/b/vox/color_guard/human/Head.vox")));
         }
@@ -126,7 +126,8 @@ public class ColorGuardAssets extends ApplicationAdapter {
         //// The latest BLUE_NOISE dither is a huge improvement, and looks much better in animations.
 //        gif.setDitherAlgorithm(Dithered.DitherAlgorithm.BLUE_NOISE);
         //// The recent changes to GRADIENT_NOISE dither finally make it usable, and it's an ordered dither, too
-        gif.setDitherAlgorithm(Dithered.DitherAlgorithm.GRADIENT_NOISE);
+//        gif.setDitherAlgorithm(Dithered.DitherAlgorithm.GRADIENT_NOISE);
+        gif.setDitherAlgorithm(Dithered.DitherAlgorithm.ROBERTS);
 //        gif.palette = new PaletteReducer(Coloring.AURORA);
         gif.palette = new PaletteReducer(); // uses AURORA, OklabCareful metric
 //        gif.palette = new PaletteReducer(PaletteReducer.YAMPED); // uses YAMPED, simplest RGB metric
@@ -140,8 +141,8 @@ public class ColorGuardAssets extends ApplicationAdapter {
 //        gif.palette = new PaletteReducer(Coloring.TATER255, Gdx.files.local("assets/TaterPreload.dat").readBytes());
 //        gif.palette = new PaletteReducer(Coloring.TETRA256, Gdx.files.local("assets/TetraPreload.dat").readBytes());
 //        gif.palette = new PaletteReducer(Coloring.BETSY256, Gdx.files.local("assets/BetsyPreload.dat").readBytes());
-        //// BLUE_NOISE doesn't need this, but NEUE and GRADIENT_NOISE do.
-        gif.palette.setDitherStrength(0.5f);
+        //// BLUE_NOISE doesn't need this, but NEUE, GRADIENT_NOISE, and ROBERTS do.
+        gif.setDitherStrength(0.5f);
         FrameBuffer fb = new FrameBuffer(Pixmap.Format.RGBA8888, Gdx.graphics.getBackBufferWidth(), Gdx.graphics.getBackBufferHeight(), false);
         ObjectIntMap<String> doneReceive = new ObjectIntMap<>(16);
         doneReceive.setDefaultValue(-1);
