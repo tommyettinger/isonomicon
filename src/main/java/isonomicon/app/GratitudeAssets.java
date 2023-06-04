6c0d6eff28d82f3c1c501145bf0be81e8ae33d33
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-06-03 20:13:49 -0700

    More markings!

--- src/main/java/isonomicon/app/GratitudeAssets.java
+++ src/main/java/isonomicon/app/GratitudeAssets.java
@@ -49,27 +49,28 @@ public class GratitudeAssets extends ApplicationAdapter {
     public GratitudeAssets() {
         VoxIOExtended.GENERAL = true;
         System.out.println("INVALID ARGUMENTS. Please supply space-separated absolute paths to .vox models, or use the .bat file.");
-        inputs = ObjectObjectOrderedMap.with("b/vox/gratitude/A24.vox", new String[]{
-                                "palettes/b/ColorGuardBaseDark.png", "Dark_Priest",
-                                "palettes/b/ColorGuardBaseWhite.png", "Light_Priest",
-                                "palettes/b/ColorGuardBaseRed.png", "War_Priest",
-                        },
-                "b/vox/gratitude/A25.vox", new String[]{
-                        "palettes/b/ColorGuardBaseDark.png", "Man_In_Black",
-                        "palettes/b/ColorGuardBaseWhite.png", "Man_In_White",
-                        "palettes/b/ColorGuardBaseRed.png", "Man_In_Red",
-                        "palettes/b/ColorGuardBaseGreen.png", "Man_In_Green",
-                        "palettes/b/ColorGuardBaseBlue.png", "Man_In_Blue",
-                },
-                "b/vox/gratitude/A26.vox", new String[]{
-                        "palettes/b/ColorGuardBaseDark.png", "Dark_Hunter",
-                        "palettes/b/ColorGuardBaseBlue.png", "Sea_Hunter",
-                },
-                "b/vox/gratitude/A27.vox", new String[]{
-                        "palettes/b/ColorGuardBaseDark.png", "Night_Warrior",
-                        "palettes/b/ColorGuardBaseYellow.png", "Mercenary_Warrior",
-                        "palettes/b/ColorGuardBaseRed.png", "Crimson_Warrior",
-                },
+        inputs = ObjectObjectOrderedMap.with(
+//                "b/vox/gratitude/A24.vox", new String[]{
+//                                "palettes/b/ColorGuardBaseDark.png", "Dark_Priest",
+//                                "palettes/b/ColorGuardBaseWhite.png", "Light_Priest",
+//                                "palettes/b/ColorGuardBaseRed.png", "War_Priest",
+//                        },
+//                "b/vox/gratitude/A25.vox", new String[]{
+//                        "palettes/b/ColorGuardBaseDark.png", "Man_In_Black",
+//                        "palettes/b/ColorGuardBaseWhite.png", "Man_In_White",
+//                        "palettes/b/ColorGuardBaseRed.png", "Man_In_Red",
+//                        "palettes/b/ColorGuardBaseGreen.png", "Man_In_Green",
+//                        "palettes/b/ColorGuardBaseBlue.png", "Man_In_Blue",
+//                },
+//                "b/vox/gratitude/A26.vox", new String[]{
+//                        "palettes/b/ColorGuardBaseDark.png", "Dark_Hunter",
+//                        "palettes/b/ColorGuardBaseBlue.png", "Sea_Hunter",
+//                },
+//                "b/vox/gratitude/A27.vox", new String[]{
+//                        "palettes/b/ColorGuardBaseDark.png", "Night_Warrior",
+//                        "palettes/b/ColorGuardBaseYellow.png", "Mercenary_Warrior",
+//                        "palettes/b/ColorGuardBaseRed.png", "Crimson_Warrior",
+//                },
                 "b/vox/gratitude/A28.vox", new String[]{
                         "palettes/b/ColorGuardBaseDark.png", "Death_Knight",
                         "palettes/b/ColorGuardBaseYellow.png", "Mercenary_Knight",
