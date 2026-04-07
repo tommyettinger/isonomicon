a6fe21942ca8eaa321bcd006c034819f0d72dbfa
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-04-06 20:30:29 -0700

    Even using max lightness darkens...
    I think it's blending with transparent black...

--- src/main/java/isonomicon/visual/SpecialRenderer.java
+++ src/main/java/isonomicon/visual/SpecialRenderer.java
@@ -499,7 +499,8 @@ public class SpecialRenderer {
 //                                128 << 16 |
 //                                128 << 8 | shade);
                         buffer.put(idx, lightIndices[x][y]);
-                        buffer.put(idx+1, (byte) Math.min(Math.max(paletteL[lightIndices[x][y] & 255] * 255.999f, 0), 255));
+                        buffer.put(idx+1, (byte) 255);
+//                        buffer.put(idx+1, (byte) Math.min(Math.max(paletteL[lightIndices[x][y] & 255] * 255.999f, 0), 255));
                         buffer.put(idx+2, (byte) 96);
                         buffer.put(idx+3, (byte) shade);
                         outlineIndices[x][y] = 0;
@@ -515,7 +516,8 @@ public class SpecialRenderer {
 //                                128 << 16 |
 //                                128 << 8 | shade);
                         buffer.put(idx, lightIndices[x][y]);
-                        buffer.put(idx+1, (byte) Math.min(Math.max(paletteL[lightIndices[x][y] & 255] * 255.999f, 0), 255));
+                        buffer.put(idx+1, (byte) 0);
+//                        buffer.put(idx+1, (byte) Math.min(Math.max(paletteL[lightIndices[x][y] & 255] * 255.999f, 0), 255));
                         buffer.put(idx+2, (byte) 96);
                         buffer.put(idx+3, (byte) shade);
                         outlineIndices[x][y] = 0;
