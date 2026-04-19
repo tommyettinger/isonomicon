91a3604995abcf200e8b715af86778d041015bb7
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-04-18 22:19:39 -0700

    I think the default light level was too bright!

--- src/main/java/isonomicon/app/ShipSpecialGenerator.java
+++ src/main/java/isonomicon/app/ShipSpecialGenerator.java
@@ -169,20 +169,19 @@ public class ShipSpecialGenerator extends ApplicationAdapter {
             pm.clear();
             if(TURNTABLE) {
                 renderer.clear();
+                voxels.grids.clear();
+                for (int j = 0; j < original.size(); j++) {
+                    voxels.grids.add(Tools3D.deepCopy(original.get(j)));
+                }
                 for (int i = 0; i < 128; i++) {
-                    if((i & 15) == 0){
-                        voxels.grids.clear();
-                        for (int j = 0; j < original.size(); j++) {
-                            voxels.grids.add(Tools3D.deepCopy(original.get(j)));
-                        }
-                    }
                     if((i & 3) == 0) {
                         for (int j = 0; j < voxels.grids.size(); j++) {
                             Stuff.evolve(Stuff.STUFFS_B, voxels.grids.get(j), i >>> 2 & 3);
                         }
                     }
-//                    renderer.drawModelSimple(voxels, i * 0x1p-7f + 0.125f, 0f, 0f, i >>> 2 & 3, 0, 0, 0);
-                    renderer.drawModelSimple(voxels, 0.5f + 0.125f, 0f, 0f, i >>> 2 & 3, 0, 0, 0);
+                    renderer.drawModelSimple(voxels, i * 0x1p-7f + 0.125f, 0f, 0f, i >>> 2 & 3, 0, 0, 0);
+                    // Use this to debug when rotation is a problem.
+//                    renderer.drawModelSimple(voxels, 0.5f + 0.125f, 0f, 0f, i >>> 2 & 3, 0, 0, 0);
                     t.draw(renderer.palettePixmap, 0, 0);
                     FrameBuffer fb = new FrameBuffer(Pixmap.Format.RGBA8888, Gdx.graphics.getBackBufferWidth(), Gdx.graphics.getBackBufferHeight(), false);
                     fb.begin();
